b8dd867faf4bb278e2a11b6fe9e41f1b791cb94f
Anderson OSJr <anderosj@gmail.com>
2023-10-10 17:20:58 -0300

    feat: insert migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from './user/interfaces/user.entity';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -14,9 +13,10 @@ import { UserEntity } from './user/interfaces/user.entity';
       host: process.env.DB_HOST,
       password: process.env.DB_PASSWORD,
       port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      synchronize: true,
+      username: process.env.DB_USERNAME,      
       entities: [`${__dirname}/**/*.entity{.js,.ts}`],
+      migrations: [`${__dirname}/migration/{.ts,*.js}`],
+      migrationsRun: true,
     }),
     UserModule],
   controllers: [],
